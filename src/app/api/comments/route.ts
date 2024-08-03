d35b9cba84400ec9d1e19eef6da1dbd64d8d4123
Hasnain190 <s20755753@gmail.com>
2024-08-03 11:20:07 -0700

    connect using postid

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,13 +1,17 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
-
+import { Prisma } from "@prisma/client";
 export async function POST(request: Request) {
   try {
-    const { content, postId }: { content: string; postId: number } = await request.json();
+    const { content, postId }: { content: string; postId: number } =
+      await request.json();
+
     const comment = await prisma.comment.create({
       data: {
-        postId,
         content,
+        post: {
+          connect: { id: postId }, // Connect using postId
+        },
       },
     });
     return NextResponse.json(comment, { status: 201 });
